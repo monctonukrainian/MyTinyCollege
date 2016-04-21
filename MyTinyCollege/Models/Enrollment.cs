873a36a8999b607859f4009c82bf6a791dad11c7
Julia <julia@monctonukrainian.com>
2016-04-21 14:30:45 -0300

    Added the database model classes

---
+++ MyTinyCollege/Models/Enrollment.cs
@@ -0,0 +1,20 @@
+﻿namespace MyTinyCollege.Models
+{
+
+    //grade enum
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+    public class Enrollment
+    {
+        public int enrollmentID { get; set; }//PK
+        public int CourseID { get; set; }//FK to course
+        public int StudentID { get; set; }//FK to student
+
+        public Grade? Grade { get; set; }//? in this case means optional (nullable)
+        public virtual Student student { get; set; }//Many enrollments to 1 student
+
+        public virtual Course course { get; set; }//many enrollments to 1 course
+    }
+}
---
+++ MyTinyCollege/Models/OfficeAssignment.cs
@@ -0,0 +1,13 @@
+﻿namespace MyTinyCollege.Models
+{
+    public class OfficeAssignment
+    {
+        public int InstructorID { get; set; }
+        public string Location { get; set; }
+
+        public virtual Instructor Instructor { get; set; }
+
+
+    }
+
+}
