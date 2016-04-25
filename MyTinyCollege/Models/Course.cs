bc6e6c29763f5e606ecc9e0e6405a8eaaf169737
Julia <julia@monctonukrainian.com>
2016-04-25 09:56:20 -0300

    Added data annotations to model

--- MyTinyCollege/Models/Course.cs
+++ MyTinyCollege/Models/Course.cs
@@ -1,12 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyTinyCollege.Models
 {
     public class Course
     {
-        public int CourseID { get; set; }
+        /*
+         * By default, the ID property will become  the Primary key of the
+         * database table that corresponds to this class. 
+         * By default the EF (Entity Framework) interprets a property
+         * that's named ID or ClassNameID as the PK.
+         * Also, this PK will have an IDENTITY Property,
+         * you can override it using the DatabaseGeneratedOpton enum:
+         * - Computed: Database generates a value when a row is inserted or updated
+         * - Identity: Database generates a value when a row is inserted
+         * - None: Database does not generate a value
+         */
+         [DatabaseGenerated(DatabaseGeneratedOption.None)]
+         [Display(Name ="Number")]
+        public int CourseID { get; set; }//PK - Note : with Identity property 
+
+        [StringLength(50, MinimumLength =3)]
         public string Title { get; set; }
+
+        [Range(0,5)]
         public int Credits { get; set; }
 
         public int DepartmentID { get; set; }//FK to department
--- MyTinyCollege/Models/Enrollment.cs
+++ MyTinyCollege/Models/Enrollment.cs
@@ -1,4 +1,6 @@
-﻿namespace MyTinyCollege.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyTinyCollege.Models
 {
 
     //grade enum
@@ -12,6 +14,7 @@
         public int CourseID { get; set; }//FK to course
         public int StudentID { get; set; }//FK to student
 
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }//? in this case means optional (nullable)
         public virtual Student student { get; set; }//Many enrollments to 1 student
 
